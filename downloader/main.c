dbe736384734df0f0518eef2de311200a12f3325
f401 <nanLianQing@outlook.com>
2022-12-04 05:29:42 +0000

    first

---
+++ downloader/main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <openssl/ssl.h>
+
+const char* REQUEST_HEADER = 
+"GET /version/1.7.10/client.jar HTTP/1.1\r\n"
+"Host: bmclapi2.bangbang93.com\r\n"
+"Accept: */*\r\n"
+"User-Agent: aa\r\n\r\n";
+
+int create_connect(const char* ip, uint16_t port) {
+	int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+	struct sockaddr_in fd_addr;
+	memset(&fd_addr, 0, sizeof(fd_addr));
+	fd_addr.sin_addr.s_addr = inet_addr(ip);
+	fd_addr.sin_family = AF_INET;
+	fd_addr.sin_port = htons(port);
+
+	printf("ip: %s, port: %d\n", ip, port);
+
+	connect(fd, (struct sockaddr* ) &fd_addr, sizeof(fd_addr));
+	return fd;
+}
+
+char* host_name_to_ip(const char* host_name) {
+	struct hostent* ip = gethostbyname(host_name);
+	return inet_ntoa(*(struct in_addr* )ip->h_addr_list[0]);
+}
+
+int main(int argc, char *argv[])
+{
+	struct timeval timeout = {5, 0};
+	int fd = create_connect(host_name_to_ip("bmclapi2.bangbang93.com"), 80);
+	char buffer[1024 * 1024];
+	memset(buffer, 0, 1024 * 1024);
+	printf("request header: %s\n", REQUEST_HEADER);
+	send(fd, REQUEST_HEADER, strlen(REQUEST_HEADER), 0);
+	printf("recvicing\n");
+	setsockopt(fd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout, sizeof(struct timeval));
+	ssize_t size = recv(fd, buffer, sizeof(buffer) - 1, 0);
+
+	printf("size: %ld, msg: %s", size, buffer);
+	
+	close(fd);
+
+
+	return 0;
+}
