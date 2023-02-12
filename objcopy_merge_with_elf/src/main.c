448cf8e423a7403e182f0427bec527f3437fe110
f401 <nanLianQing@outlook.com>
2023-02-12 09:27:53 +0000

    update

--- objcopy_merge_with_elf/src/main.c
+++ objcopy_merge_with_elf/src/main.c
@@ -2,8 +2,7 @@
 
 static void destroy() { gtk_main_quit(); }
 
-extern int _binary_my_png_size;
-extern void *_binary_my_png_start;
+extern char _binary_my_png_start[], _binary_my_png_size[];
 
 int main(int argc, char *argv[]) {
   GtkWidget *window, *image;
@@ -12,15 +11,13 @@ int main(int argc, char *argv[]) {
   // image = gtk_image_new_from_file("../my.png");
   // GCancellable* cancellable = g_cancellable_new();
 
-  GInputStream stream;
-  g_input_stream_read(&stream, _binary_my_png_start, _binary_my_png_size, NULL,
-                      NULL);
-
-  image = gtk_image_new_from_pixbuf(
-      gdk_pixbuf_new_from_stream(&stream, NULL, NULL));
+  GInputStream *stream = g_memory_input_stream_new_from_data(
+      _binary_my_png_start, (gsize)_binary_my_png_size, NULL);
+  image =
+      gtk_image_new_from_pixbuf(gdk_pixbuf_new_from_stream(stream, NULL, NULL));
   g_signal_connect(GTK_WINDOW(window), "destroy", destroy, NULL);
 
-  g_input_stream_close(&stream, NULL, NULL);
+  g_input_stream_close(stream, NULL, NULL);
   gtk_container_add(GTK_CONTAINER(window), image);
   gtk_window_set_resizable(GTK_WINDOW(window), TRUE);
   gtk_widget_set_size_request(window, 800, 600);
